ea1f18ea1326581dd2db14ba8a9fa042abb703f0
zakaria <zakariasamy10@gmail.com>
2021-08-24 16:12:12 +0200

    php task 1

---
+++ 13/task.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+	
+    
+    for($i=1; $i<=6; $i++){
+    	for($j=1; $j<=$i; $j++)
+        	echo $i;
+        
+        echo "<br>";
+    	
+    }
+?> 
+
+</body>
+</html>
+
